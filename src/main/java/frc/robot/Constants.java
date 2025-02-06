12daa1294967d2b2f0a116f7c96b12847378fb2f
sullyrobotics <fotherstuf@gmail.com>
2025-02-06 10:40:17 -0800

    New stuffs

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,15 +5,49 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
+  public static final class DriveConstants {
+    public static final int M_L1 = 1;
+    public static final int M_L2 = 2;
+    public static final int M_R1 = 3;
+    public static final int M_R2 = 4;
+
+    public static final int CurrentLimit = 50;
+    public static final double ROB_Vol = 12;
+    public static final double SLOW_MODE_MOVE = 0.5;
+    public static final double SLOW_MODE_TURN = 0.6;
+  }
+
+  public static final class RollerConstants {
+    public static final int M_RL = 5;
+    public static final int CurrentLimit = 50;
+    public static final double ROB_Vol = 12;
+    public static final double OUTCRL = -.4;
+    public static final double INCRL = -0.8;
+  }
+
+  
+  public static final class ClimberConstants {
+    public static final int M_CL = 7;
+    public static final int CurrentLimit = 50;
+    public static final double ROB_Vol = 12;
+    public static final double CLIMBER_SPEED_DOWN = -0.5;
+    public static final double CLIMBER_SPEED_UP = 0.5;
+  }
+
+  public static final class OperatorConstants {
+    public static final int DRIVECON = 0;
+    public static final int OPCON = 1;
   }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.hal.FRCNetComm.tResourceType;
 
 /**
  * The methods in this class are called automatically corresponding to each mode, as described in
@@ -16,16 +18,22 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
-  private final RobotContainer m_robotContainer;
+  private RobotContainer m_robotContainer;
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
-  public Robot() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
+  @Override
+  public void robotInit() {
+    // Instantiate our RobotContainer. This will perform all our button bindings,
+    // and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+       
+    // Used to track usage of the Everybot code, please do not remove!
+    HAL.report(tResourceType.kResourceType_Framework, 11);
   }
 
   /**
