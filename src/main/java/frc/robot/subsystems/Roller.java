d544b55d5e1fab06c46fc390a519b1e678674da8
sullyrobotics <fotherstuf@gmail.com>
2025-02-06 09:48:05 -0800

    first commit

---
+++ src/main/java/frc/robot/subsystems/Roller.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+
+public class Roller {
+
+}
